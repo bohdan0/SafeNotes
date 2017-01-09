f03701d340f759522148ecc3661d8f5318db9edc
Bohdan Nakonechnyi <bnakonechnuy0@gmail.com>
2017-01-08 22:53:00 -0800

    add webpack.config, package.json, entry file

---
+++ frontend/safe_notes.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
+  ReactDOM.render(<h1>This is SafeNotes!!!</h1>, root);
+});
