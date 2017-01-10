e7878bb6120d4a67856912ad6a46b31d0a678fec
Bohdan Nakonechnyi <bnakonechnuy0@gmail.com>
2017-01-10 13:43:43 -0800

    add session and root reducers

---
+++ frontend/reducers/root_reducer.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+
+import SessionReducer from './session_reducer';
+
+const RootReducer = combineReducers({
+  currentUser: SessionReducer
+});
+
+export default RootReducer;
---
+++ frontend/reducers/session_reducer.js
@@ -0,0 +1,25 @@
+import merge from 'lodash/merge';
+
+import {
+  RECEIVE_CURRENT_USER,
+  LOGOUT } from '../actions/session_actions';
+
+const _nullUser = Object.freeze({
+  id: null,
+  username: null
+});
+
+const SessionReducer = (state = _nullUser, action) => {
+  Object.freeze(state);
+
+  switch(action.type) {
+    case RECEIVE_CURRENT_USER:
+      return action.currentUser;
+    case LOGOUT:
+      return _nullUser;
+    default:
+      return state;
+  }
+};
+
+export default SessionReducer;
