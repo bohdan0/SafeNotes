2034285aa5acd14f1394133cb695da942605305b
Bohdan Nakonechnyi <bnakonechnuy0@gmail.com>
2017-01-11 14:55:25 -0800

    move logout login from home to sidebar

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -1,23 +1,21 @@
 import React from 'react';
 
+import SidebarContainer from '../sidebar/sidebar_container';
+
 class Home extends React.Component {
   constructor(props) {
     super(props);
-    this.logout = this.logout.bind(this);
-  }
-
-  logout() {
-    this.props.logout()
-      .then(() => this.props.router.push('/'));
   }
 
   render() {
     return (
-      <div>
-        <h1>HOME</h1>
-        <p>Hello {this.props.currentUser.username}!</p>
-        <button type="button"
-                onClick={this.logout}>Log Out</button>
+      <div className='home'>
+        <SidebarContainer router={ this.props.router }/>
+
+        <main className='main'>
+          <h1>HOME</h1>
+          <p>Hello {this.props.currentUser.username}!</p>
+        </main>
       </div>
     );
   }
--- frontend/components/home/home_container.jsx
+++ frontend/components/home/home_container.jsx
@@ -1,6 +1,5 @@
 import { connect } from 'react-redux';
 
-import { logout } from '../../actions/session_actions';
 import Home from './home';
 
 const mapStateToProps = ({ currentUser }) => ({
@@ -8,7 +7,7 @@ const mapStateToProps = ({ currentUser }) => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-  logout: () => dispatch(logout())
+  
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Home);
