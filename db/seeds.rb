d93c93c191260908ae5281e0cec601c53af246b1
Bohdan Nakonechnyi <bnakonechnuy0@gmail.com>
2017-01-11 19:04:18 -0800

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,24 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.create(username: 'Guest', password: 'password')
+
+user1 = User.create(username: 'user1', password: 'password')
+user2 = User.create(username: 'user2', password: 'password')
+user3 = User.create(username: 'user3', password: 'password')
+
+3.times do |i|
+  Notebook.create(title: "notebook #{i + 1}", author_id: i + 1)
+end
+
+(1..3).each do |idx|
+  Note.create(title: "note 1", body: "body", author_id: idx, notebook_id: idx)
+end
+
+Tag.create(name: 'cool', author_id: 1)
+Tag.create(name: 'ruby', author_id: 1)
+Tag.create(name: 'nope', author_id: 2)
+Tag.create(name: 'ok', author_id: 3)
+
+Tagging.create(tag_id: 1, note_id: 1)
+Tagging.create(tag_id: 2, note_id: 2)
+Tagging.create(tag_id: 3, note_id: 3)
