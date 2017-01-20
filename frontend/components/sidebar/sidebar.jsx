c90d5cca219ce311377cf9b471f19d2d9cf95784
Bohdan Nakonechnyi <bnakonechnuy0@gmail.com>
2017-01-19 19:50:16 -0800

    add tooltip for sidebar buttons

--- frontend/components/sidebar/sidebar.jsx
+++ frontend/components/sidebar/sidebar.jsx
@@ -5,15 +5,17 @@ const Sidebar = ({ logout }) => (
   <div>
     <div className='sidebar'>
       <div className='sidebar-top'>
-        <img src="http://res.cloudinary.com/safenotes/image/upload/v1484796892/1484384030_Vector-icons_39_zzk1ov.png"
-              alt="Logo" 
-              className="logo"/>
+        <img alt="Logo"
+             src="http://res.cloudinary.com/safenotes/image/upload/v1484796892/1484384030_Vector-icons_39_zzk1ov.png"
+             className="logo"/>
 
         <Link to='/home/new/note'>
           <img className='sidebar-btn side-new'
                alt='new_note'
                src='http://res.cloudinary.com/safenotes/image/upload/v1484796893/circular-plus-button_oiuvj4.png'
-               data-toggle='tooltip' title='NEW NOTE'/>
+               data-toggle='tooltip' 
+               title='NEW NOTE'
+               />
         </Link>
       </div>
 
@@ -22,7 +24,9 @@ const Sidebar = ({ logout }) => (
           <Link to='/home/notebooks/all/notes/all'>
             <img className='side-notes'
                  src='http://res.cloudinary.com/safenotes/image/upload/v1484796893/document-with-text-lines-_zktmln.png'
-                 data-toggle='tooltip' title='NOTES' data-placement='right'/>
+                 data-toggle='tooltip' 
+                 title='NOTES' 
+                 data-placement='right'/>
           </Link>
         </li>
 
@@ -31,7 +35,9 @@ const Sidebar = ({ logout }) => (
             <img className='side-notebooks'
                  alt='notebooks'
                  src='http://res.cloudinary.com/safenotes/image/upload/v1484797097/book-with-bookmarker_qdlxyq.png'
-                 data-toggle='tooltip' title='NOTEBOOKS' data-placement='right'/>
+                 data-toggle='tooltip' 
+                 title='NOTEBOOKS' 
+                 data-placement='right'/>
           </Link>
         </li>
 
@@ -40,7 +46,9 @@ const Sidebar = ({ logout }) => (
             <img className='side-tags'
                  alt='tags'
                  src='http://res.cloudinary.com/safenotes/image/upload/v1484796893/clothes-tag_p1qydq.png'
-                 data-toggle='tooltip' title='TAGS' data-placement='right'/>
+                 data-toggle='tooltip' 
+                 title='TAGS' 
+                 data-placement='right'/>
           </Link>
         </li>
       </ul>
@@ -49,7 +57,9 @@ const Sidebar = ({ logout }) => (
             onClick={ logout }
             src="http://res.cloudinary.com/safenotes/image/upload/v1484796893/circular-power-on-button_xpm2sy.png" 
             alt="Log Out"
-            data-toggle='tooltip' title='LOGOUT' data-placement='right'/>
+            data-toggle='tooltip' 
+            title='LOGOUT' 
+            data-placement='right'/>
     </div>
   </div>
 );
