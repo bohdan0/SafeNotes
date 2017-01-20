1b725da5fe35ff66deb13d8a14004f1d3858de12
Bohdan Nakonechnyi <bnakonechnuy0@gmail.com>
2017-01-20 13:34:51 -0800

    fix warning on heroku

---
+++ webpack.config.prod.js
@@ -0,0 +1,40 @@
+const webpack = require("webpack");
+const path = require('path');
+
+module.exports = {
+  context: __dirname,
+  entry: './frontend/safe_notes.jsx',
+  output: {
+    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  plugins:[
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: true
+      }
+    })
+  ],
+  module: {
+    noParse: /node_modules\/quill\/dist/,
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
+  devtool: 'source-maps'
+};
