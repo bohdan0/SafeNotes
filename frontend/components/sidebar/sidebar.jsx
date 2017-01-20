d2bedd5c091cdde51416304872e296d48e11941e
Bohdan Nakonechnyi <bnakonechnuy0@gmail.com>
2017-01-20 10:29:22 -0800

    add tooltip for sidebar buttons

--- frontend/components/sidebar/sidebar.jsx
+++ frontend/components/sidebar/sidebar.jsx
@@ -1,3 +1,4 @@
+import ReactTooltip from 'react-tooltip';
 import { Link } from 'react-router';
 import React from 'react';
 
@@ -11,55 +12,87 @@ const Sidebar = ({ logout }) => (
 
         <Link to='/home/new/note'>
           <img className='sidebar-btn side-new'
+               data-tip='React-tooltip'
+               data-for='new_note'
                alt='new_note'
-               src='http://res.cloudinary.com/safenotes/image/upload/v1484796893/circular-plus-button_oiuvj4.png'
-               data-toggle='tooltip' 
-               title='NEW NOTE'
-               />
+               src='http://res.cloudinary.com/safenotes/image/upload/v1484796893/circular-plus-button_oiuvj4.png'/>
         </Link>
+
+        <ReactTooltip id='new_note' 
+                      place='right' 
+                      type='dark' 
+                      effect='solid' 
+                      offset={ { right: 11, bottom: 3 } }>
+          <span>NEW NOTE</span>
+        </ReactTooltip>
       </div>
 
       <ul className='nav'>
         <li className='sidebar-btn'>
           <Link to='/home/notebooks/all/notes/all'>
             <img className='side-notes'
-                 src='http://res.cloudinary.com/safenotes/image/upload/v1484796893/document-with-text-lines-_zktmln.png'
-                 data-toggle='tooltip' 
-                 title='NOTES' 
-                 data-placement='right'/>
+                 data-tip='React-tooltip'
+                 data-for='notes'
+                 alt='notes'
+                 src='http://res.cloudinary.com/safenotes/image/upload/v1484796893/document-with-text-lines-_zktmln.png'/>
           </Link>
+
+          <ReactTooltip id='notes' 
+                        place='right' 
+                        type='dark' 
+                        effect='solid'>
+            <span>NOTES</span>
+          </ReactTooltip>
         </li>
 
         <li className='sidebar-btn'>
           <Link to='/home/notebooks/all'>
             <img className='side-notebooks'
+                 data-tip='React-tooltip'
+                 data-for='notebooks'
                  alt='notebooks'
-                 src='http://res.cloudinary.com/safenotes/image/upload/v1484797097/book-with-bookmarker_qdlxyq.png'
-                 data-toggle='tooltip' 
-                 title='NOTEBOOKS' 
-                 data-placement='right'/>
+                 src='http://res.cloudinary.com/safenotes/image/upload/v1484797097/book-with-bookmarker_qdlxyq.png'/>
           </Link>
+
+          <ReactTooltip id='notebooks' 
+                        place='right' 
+                        type='dark' 
+                        effect='solid'>
+            <span>NOTEBOOKS</span>
+          </ReactTooltip>
         </li>
 
         <li className='sidebar-btn'>
           <Link to='/home/tags/all'>
             <img className='side-tags'
+                 data-tip='React-tooltip'
+                 data-for='tags'
                  alt='tags'
-                 src='http://res.cloudinary.com/safenotes/image/upload/v1484796893/clothes-tag_p1qydq.png'
-                 data-toggle='tooltip' 
-                 title='TAGS' 
-                 data-placement='right'/>
+                 src='http://res.cloudinary.com/safenotes/image/upload/v1484796893/clothes-tag_p1qydq.png'/>
           </Link>
+
+          <ReactTooltip id='tags' 
+                        place='right' 
+                        type='dark' 
+                        effect='solid'>
+            <span>TAGS</span>
+          </ReactTooltip>
         </li>
       </ul>
 
       <img className='sidebar-btn side-logout'
-            onClick={ logout }
-            src="http://res.cloudinary.com/safenotes/image/upload/v1484796893/circular-power-on-button_xpm2sy.png" 
-            alt="Log Out"
-            data-toggle='tooltip' 
-            title='LOGOUT' 
-            data-placement='right'/>
+           data-tip='React-tooltip'
+           data-for='logout'
+           onClick={ logout }
+           alt="log_out"
+           src="http://res.cloudinary.com/safenotes/image/upload/v1484796893/circular-power-on-button_xpm2sy.png"/>
+
+      <ReactTooltip id='logout' 
+                    place='right' 
+                    type='dark' 
+                    effect='solid'>
+        <span>LOG OUT</span>
+      </ReactTooltip>
     </div>
   </div>
 );
